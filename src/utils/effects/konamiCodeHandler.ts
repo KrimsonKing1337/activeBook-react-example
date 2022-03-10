a2eb60384b57f40ed1129841bf11cbd4088a8332
Oleg Shilov <shilov-1@yandex.ru>
2022-03-10 16:20:11 +0300

    added konami code handler;

---
+++ src/utils/effects/konamiCodeHandler.ts
@@ -0,0 +1,32 @@
+export function konamiCodeHandler(cb: () => any) {
+  const pattern = [
+    'ArrowUp',
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowDown',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowRight',
+    'b',
+    'a',
+  ];
+
+  let current = 0;
+
+  return (e: KeyboardEvent) => {
+    if (pattern.indexOf(e.key) < 0 || e.key !== pattern[current]) {
+      current = 0;
+
+      return;
+    }
+
+    current++;
+
+    if (pattern.length === current) {
+      current = 0;
+
+      cb();
+    }
+  };
+}
