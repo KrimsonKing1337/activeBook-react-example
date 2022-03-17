cf250627d2bdb0356f7db58b46c01c6af68b271d
Oleg Shilov <shilov-1@yandex.ru>
2022-03-17 09:54:22 +0300

    removed developments leftover from utils/flashlight;

--- src/utils/effects/flashlight.ts
+++ src/utils/effects/flashlight.ts
@@ -1,15 +1,7 @@
 export function on() {
-  console.log('___ FLASH');
-
-  return;
-
   (window as any).plugins.flashlight.switchOn();
 }
 
 export function off() {
-  console.clear();
-
-  return;
-
   (window as any).plugins.flashlight.switchOff();
 }
