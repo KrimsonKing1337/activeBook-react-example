baa40906383dc5db2361f3b8ae400e5b4a8ea648
Oleg Shilov <shilov-1@yandex.ru>
2022-06-24 09:05:06 +0300

    fixed styles of modal in page 31;

--- src/book_pages/Page31/hooks.ts
+++ src/book_pages/Page31/hooks.ts
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const localStorageFlag = 'modalWithCommentsAboutInverseColorWasShowed';
 
 export function useModal() {
   const initValue = !localStorage.getItem(localStorageFlag);
 
-  const [modalIsActive, setModalIsActive] = useState(initValue);
+  const [modalIsActive, setModalIsActive] = useState(false);
+
+  /**
+   * я здесь применяю initValue, а не в useState выше,
+   * потому что иначе не сработает componentDidUpdate,
+   * в котором происходит применение стилей для иконок
+   * и изменение адреса.
+   * пока лень переделывать из-за одного случая.
+   * мб позже вернусь к этому
+   */
+  useEffect(() => {
+    if (initValue) {
+      setModalIsActive(true);
+    }
+  }, []);
 
   const modalOnClose = () => {
     setModalIsActive(false);
