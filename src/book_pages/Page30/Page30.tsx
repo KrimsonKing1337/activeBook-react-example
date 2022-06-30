a9963fd03f31a59e284b12cc09cdf69b810776e6
Oleg Shilov <shilov-1@yandex.ru>
2022-06-30 19:30:15 +0300

    fixed text; added music for page-30;

--- src/book_pages/Page30/Page30.tsx
+++ src/book_pages/Page30/Page30.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 import { PageWrapper } from 'components/PageWrapper';
 
+import { useMusic } from 'hooks/effects/audio/music';
+
 export const Page30 = () => {
+  useMusic({
+    src: '/assets/book_data/audios/music/Promise_From_Distant_Days.mp3',
+    loop: true,
+  });
+
   return (
     <PageWrapper>
       <p>
