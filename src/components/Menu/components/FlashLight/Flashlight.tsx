f119ba20da994291ca7777f241d77ccddd4c6c51
Oleg Shilov <shilov-1@yandex.ru>
2022-07-07 15:36:41 +0300

    fixed Flashlight and Vibration;

--- src/components/Menu/components/FlashLight/Flashlight.tsx
+++ src/components/Menu/components/FlashLight/Flashlight.tsx
@@ -2,23 +2,37 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { configActions, configSelectors } from 'store/config';
+import { mainSelectors } from 'store/main';
 
 import { Toggle } from 'components/Toggle';
 import { playAchievement } from 'components/Menu/utils';
 
+import { off as flashlightOff, on as flashlightOn } from 'utils/effects/flashlight';
+import { sleep } from 'utils/sleep';
+
 export const Flashlight = () => {
   const dispatch = useDispatch();
-  const flashlightState = useSelector(configSelectors.flashlight);
 
-  const toggleClickHandler = (value: boolean) => {
+  const flashlightState = useSelector(configSelectors.flashlight);
+  const isFlashlightAvailable = useSelector(mainSelectors.isFlashlightAvailable);
+
+  const toggleClickHandler = async (value: boolean) => {
     dispatch(configActions.setFlashlight(value));
 
     playAchievement();
+
+    if (value && isFlashlightAvailable) {
+      flashlightOn();
+
+      await sleep(300);
+
+      flashlightOff();
+    }
   };
 
   return (
     <Toggle
-      label={'Вспышка'}
+      label={'Вспышка (там, где доступно)'}
       isActiveDefault={flashlightState}
       onClickOn={() => toggleClickHandler(true)}
       onClickOff={() => toggleClickHandler(false)}
--- src/components/Menu/components/Vibration/Vibration.tsx
+++ src/components/Menu/components/Vibration/Vibration.tsx
@@ -2,27 +2,36 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { configActions, configSelectors } from 'store/config';
+import { mainSelectors } from 'store/main';
 
 import { Toggle } from 'components/Toggle';
 import { Spoiler } from 'components/Spoiler';
 import { playAchievement } from 'components/Menu/utils';
 
+import { on as vibrationOn } from 'utils/effects/vibration';
+
 import styles from './Vibration.scss';
 
 export const Vibration = () => {
   const dispatch = useDispatch();
+
   const vibrationState = useSelector(configSelectors.vibration);
+  const isVibrationAvailable = useSelector(mainSelectors.isVibrationAvailable);
 
   const toggleClickHandler = (value: boolean) => {
     dispatch(configActions.setVibration(value));
 
     playAchievement();
+
+    if (value && isVibrationAvailable) {
+      vibrationOn(300);
+    }
   };
 
   return (
     <div className={styles.vibration}>
       <Toggle
-        label={'Вибрация'}
+        label={'Вибрация (там, где доступно)'}
         isActiveDefault={vibrationState}
         withoutBorder={true}
         onClickOn={() => toggleClickHandler(true)}
@@ -30,7 +39,7 @@ export const Vibration = () => {
       />
 
       <Spoiler label="Не работает вибрация?">
-        Проверьте, что у вас не включён беззвучный режим.
+        Проверьте, что ваше устройство поддерживает вибрацию, и что не включён беззвучный режим.
         Часто он не даёт вибрации срабатывать
       </Spoiler>
     </div>
