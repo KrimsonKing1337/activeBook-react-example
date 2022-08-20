c5c0d6f8c54c633a1a1eb1c622fab99cbae03cd8
Oleg Shilov <shilov-1@yandex.ru>
2022-08-20 14:16:58 +0300

    made AuthorComment trigger more dark; refactored styles;

--- src/utils/styles/setThemeCss.tsx
+++ src/utils/styles/setThemeCss.tsx
@@ -19,28 +19,28 @@ export function setThemeCss(theme: Theme) {
       secondary: '#828282',
       hover: '#7a7a7a',
       bg: '#111',
-      textShadow: '#ffffff7f',
+      'text-shadow-for-hover': '#ffffff7f',
     },
     orange: {
       main: '#f2994a',
       secondary: '#000',
       hover: '#f2c94c',
       bg: '#fff',
-      textShadow: '#1111117f',
+      'text-shadow-for-hover': '#1111117f',
     },
     darkBlue: {
       main: '#728c9a',
       secondary: '#000',
       hover: '#9bc4d9',
       bg: '#fff',
-      textShadow: '#1111117f',
+      'text-shadow-for-hover': '#1111117f',
     },
     black: {
       main: '#111',
       secondary: '#000',
       hover: '#a3a3a3',
       bg: '#fff',
-      textShadow: '#1111117f',
+      'text-shadow-for-hover': '#1111117f',
     },
   };
 
