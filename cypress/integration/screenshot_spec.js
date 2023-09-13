c17828b7f354c7dc6e9ca0262b640f1b8fdff246
Oleg Shilov <shilov-1@yandex.ru>
2023-09-13 15:39:49 +0300

    removed storybook and cypress;

--- cypress/integration/screenshot_spec.js
+++
@@ -1,9 +0,0 @@
-describe('screenshot', () => {
-  it('takes a screenshot of a chat\'s default opened window', () => {
-    cy.visit('http:localhost:3000');
-
-    cy.get('button', {timeout: 1000}).click();
-    cy.document().its('fonts.status').should('equal', 'loaded');
-    cy.get('body').toMatchImageSnapshot({name: 'default', threshold: 0.001});
-  });
-});
